39fb3276c7010e266eba3f924ca8e70dc0436001
NeilK15 <neiltkapur@gmail.com>
2024-06-10 23:30:42 -0400

    Minor changes and bug fixes, mainly re-familiarizing myself with the code

--- kapur-home-client/lib/api.ts
+++ kapur-home-client/lib/api.ts
@@ -1,31 +1,31 @@
 import { RecipeData } from "@/app/recipes/[id]/@customTypes/RecipeTypes";
 
 export async function getRecipeById(id: number): Promise<RecipeData> {
-  const res = await fetch(`http://${process.env.API_URI}/recipes/fetchRecipeById?id=${id}`, { cache: "no-cache" });
+    const res = await fetch(`http://${process.env.API_URI}/recipes/${id}`, { cache: "no-cache" });
 
-  if (!res.ok) {
-    throw new Error(`failed to fetch recipe data with id: ${id}`);
-  }
+    if (!res.ok) {
+        throw new Error(`failed to fetch recipe data with id: ${id}`);
+    }
 
-  const rawData = await res.json();
+    const rawData = await res.json();
 
-  return new Promise((resolve) => {
-    resolve(rawData as RecipeData);
-  });
+    return new Promise((resolve) => {
+        resolve(rawData as RecipeData);
+    });
 }
 
 export async function getRecipes(filter: any, limit: number): Promise<RecipeData[]> {
-  const res = await fetch(`http://${process.env.API_URI}/recipes?filter=${filter}&limit=${limit}`, {
-    cache: "no-cache",
-  });
+    const res = await fetch(`http://${process.env.API_URI}/recipes?filter=${filter}&limit=${limit}`, {
+        cache: "no-cache",
+    });
 
-  if (!res.ok) {
-    throw new Error(`failed to fetch recipes`);
-  }
+    if (!res.ok) {
+        throw new Error(`failed to fetch recipes`);
+    }
 
-  const rawData = await res.json();
+    const rawData = await res.json();
 
-  return new Promise((resolve) => {
-    resolve(rawData as RecipeData[]);
-  });
+    return new Promise((resolve) => {
+        resolve(rawData as RecipeData[]);
+    });
 }
---
+++ kapur-home-client/src/app/recipes/add/add-by-url/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { addRecipeByUrl } from "@/app/request-handler";
+import "../../css/recipes-view.css";
+import { Url } from "next/dist/shared/lib/router/router";
+import { useState } from "react";
+import Recipe from "../../[id]/components/Recipe";
+import { RecipeData } from "../../[id]/@customTypes/RecipeTypes";
+
+const Page = () => {
+    const [hasRecipe, setHasRecipe] = useState(false);
+    const [recipeData, setRecipeData] = useState<RecipeData>();
+
+    const handleSubmit = (data: FormData) => {
+        addRecipeByUrl(data.get("recipeUrl") as Url).then((recipeData) => {
+            setHasRecipe(true);
+            setRecipeData(recipeData);
+        });
+    };
+
+    const addRecipeJSX = (
+        <>
+            <h1>Here you will be able to add a recipe</h1>
+            <form action={handleSubmit}>
+                <input type="text" name="recipeUrl" id="recipeUrl" />
+                <input type="submit" value="submit" />
+            </form>
+        </>
+    );
+
+    const recipePreview = (
+        <>
+            <h1>Preview</h1>
+            <Recipe recipeData={recipeData as RecipeData}></Recipe>
+        </>
+    );
+
+    return <main>{hasRecipe ? recipePreview : addRecipeJSX}</main>;
+};
+
+export default Page;

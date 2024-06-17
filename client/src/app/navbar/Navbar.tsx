e3f328728f9b1deb82b91dc1b57f24e649ed04f7
NeilK15 <neiltkapur@gmail.com>
2024-06-16 22:13:32 -0400

    I've decided I dont like next js im gonna stick to MERN. committing some changes, next commit will be migrating back to REACT hehe

--- client/src/app/navbar/Navbar.tsx
+++ client/src/app/navbar/Navbar.tsx
@@ -7,89 +7,89 @@ import "./navbar.css";
 import { Inter } from "next/font/google";
 
 const Navbar = () => {
-  const recipeSubMenus = [
-    {
-      key: "2a",
-      href: "/recipes/add",
-      children: "Add",
-    },
-  ];
+    const recipeSubMenus = [
+        {
+            key: "2a",
+            href: "/recipes/add",
+            children: "Add",
+        },
+    ];
 
-  return (
-    <nav>
-      <ul className="navbar">
-        <NavButton key={1} href={"/"} className="navbar__nav_button--first">
-          <img src="/chewie-outline.png" alt="Chewbarka" className="navbar__nav_buton__img"></img>
-        </NavButton>
-        <NavDropdown key={2} href={"/recipes"} subMenus={recipeSubMenus}>
-          Cookbook +
-        </NavDropdown>
-        <NavButton key={3} href={"/chat"}>
-          Chat
-        </NavButton>
-        <NavButton key={4} href={"/about"}>
-          About
-        </NavButton>
-        <SearchBar />
-      </ul>
-    </nav>
-  );
+    return (
+        <nav>
+            <ul className="navbar">
+                <NavButton key={1} href={"/"} className="navbar__nav_button--first">
+                    <img src="/chewie-outline.png" alt="Chewbarka" className="navbar__nav_buton__img"></img>
+                </NavButton>
+                <NavDropdown key={2} href={"/recipes"} subMenus={recipeSubMenus}>
+                    Cookbook
+                </NavDropdown>
+                <NavButton key={3} href={"/chat"}>
+                    Chat
+                </NavButton>
+                <NavButton key={4} href={"/about"}>
+                    About
+                </NavButton>
+                <SearchBar />
+            </ul>
+        </nav>
+    );
 };
 
 type NavButtonProps = {
-  key?: string | number;
-  href: Url;
-  className?: string;
-  children: React.ReactNode;
+    key?: string | number;
+    href: Url;
+    className?: string;
+    children: React.ReactNode;
 };
 
 const NavButton = ({ key = -1, href, className = "", children }: NavButtonProps) => {
-  return (
-    <>
-      {key !== -1 ? (
-        <li className="navbar__item" key={key}>
-          <Link href={href} className={`navbar__nav_button ${className}`}>
-            {children}
-          </Link>
-        </li>
-      ) : (
-        <Link href={href} className={`navbar__nav_button ${className}`}>
-          {children}
-        </Link>
-      )}
-    </>
-  );
+    return (
+        <>
+            {key !== -1 ? (
+                <li className="navbar__item" key={key}>
+                    <Link href={href} className={`navbar__nav_button ${className}`}>
+                        {children}
+                    </Link>
+                </li>
+            ) : (
+                <Link href={href} className={`navbar__nav_button ${className}`}>
+                    {children}
+                </Link>
+            )}
+        </>
+    );
 };
 
 type NavDropdownProps = {
-  key: string | number;
-  href: Url;
-  subMenus: Array<NavButtonProps>;
-  children: React.ReactNode;
+    key: string | number;
+    href: Url;
+    subMenus: Array<NavButtonProps>;
+    children: React.ReactNode;
 };
 
 const NavDropdown = ({ key, href, subMenus, children }: NavDropdownProps) => {
-  const subMenuComponents = subMenus.map((menus) => (
-    <NavButton key={menus.key} href={menus.href} className="dropdown__item">
-      {menus.children}
-    </NavButton>
-  ));
+    const subMenuComponents = subMenus.map((menus) => (
+        <NavButton key={menus.key} href={menus.href} className="dropdown__item">
+            {menus.children}
+        </NavButton>
+    ));
 
-  return (
-    <li className="navbar__dropdown navbar__item">
-      <NavButton href={href}>{children}</NavButton>
-      <ul className="dropdown__items">{subMenuComponents}</ul>
-    </li>
-  );
+    return (
+        <li className="navbar__dropdown navbar__item">
+            <NavButton href={href}>{children}</NavButton>
+            <ul className="dropdown__items">{subMenuComponents}</ul>
+        </li>
+    );
 };
 
 const SearchBar = () => {
-  return (
-    <li key={10} className="navbar__search_item">
-      <input type="search" name="search" id="search" className="navbar__search_bar" autoComplete="off" />
-      <input type="submit" value="Search" className="search_bar__suffix" />
-    </li>
-  );
+    return (
+        <li key={10} className="navbar__search_item">
+            <input type="search" name="search" id="search" className="navbar__search_bar" autoComplete="off" />
+            <input type="submit" value="Search" className="search_bar__suffix" />
+        </li>
+    );
 };
 
 export default Navbar;
--- client/src/app/recipes/add/add-manually/page.tsx
+++ client/src/app/recipes/add/add-manually/page.tsx
@@ -1,9 +1,73 @@
+"use client";
+
 import { NextPage } from "next";
+import { addRecipeByUrl } from "@/app/request-handler";
+import "../../css/recipes-view.css";
+import { Url } from "next/dist/shared/lib/router/router";
+import { useState } from "react";
+import Recipe from "../../[id]/components/Recipe";
+import { RecipeData } from "../../[id]/@customTypes/RecipeTypes";
 
 interface Props {}
 
 const Page: NextPage<Props> = ({}) => {
-    return <div>This is working</div>;
+    const [hasRecipe, setHasRecipe] = useState(false);
+    const [recipeData, setRecipeData] = useState<RecipeData>();
+
+    const handleSubmit = (data: FormData) => {
+        addRecipeByUrl(data.get("recipeUrl") as Url).then((recipeData) => {
+            setHasRecipe(true);
+            setRecipeData(recipeData);
+        });
+    };
+
+    const addRecipeJSX = (
+        <>
+            <h1>Here you will be able to add a recipe</h1>
+            <form action={handleSubmit}>
+                <div className="recipe flex_recipe">
+                    <div className="recipe__header_and_image">
+                        <div className="recipe__info">
+                            <h1 className="recipe__info__title">
+                                {/* <textarea className="recipe__info__title" name="recipeUrl" id="recipeUrl" /> */}
+                                <span contentEditable="true" className="recipe__info__title"></span>
+                            </h1>
+                            {/* <Details {...detailsData} /> */}
+                        </div>
+                        {/* <img src={recipeData.imageUrl} alt={`Image of ${recipeData.name}`} className="recipe__image" /> */}
+                    </div>
+
+                    {/* {recipeData.description && <Description>{recipeData.description}</Description>} */}
+
+                    {/* {ingredientGroups.length > 0 && (
+                        <div className="ingredients_container">
+                            <Header>Ingredients</Header>
+                            <ul className="ingredients_groups">{ingredientGroups}</ul>
+                        </div>
+                    )} */}
+
+                    {/* {instructionGroups.length > 0 && (
+                        <div className="instructions_container">
+                            <Header>Instructions</Header>
+                            <ol className="instructions_groups">{instructionGroups}</ol>
+                        </div>
+                    )} */}
+
+                    {/* {nutritionData && <Nutrition nutritionData={nutritionData} />} */}
+                    <input type="submit" value="submit" />
+                </div>
+            </form>
+        </>
+    );
+
+    const recipePreview = (
+        <>
+            <h1>Preview</h1>
+            <Recipe recipeData={recipeData as RecipeData}></Recipe>
+        </>
+    );
+
+    return <main>{hasRecipe ? recipePreview : addRecipeJSX}</main>;
 };
 
 export default Page;
